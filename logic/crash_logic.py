d4b548de0069e103889bf328638cd895ddb482ae
ITANITI PENGJAEM <6810110432@psu.ac.th>
2026-02-27 00:20:50 +0700

    [Crash System] logic/crash_logic.py เพิ่ม ฟังก์ชัน resolve_crash เพื่อเปรียบเทียบแต้มรวมระหว่างผู้เล่น 2 ฝ่ายและประกาศผลแพ้ชนะ

--- logic/crash_logic.py
+++ logic/crash_logic.py
@@ -27,7 +27,7 @@ def toss_coin():
     else:
         return 1, "หัวสีเหลือง"
 
-        
+
 def calculate_total_points(base_points, num_coins):
     """
     คำนวณแต้มรวมทั้งหมดของตัวละคร
@@ -41,3 +41,41 @@ def calculate_total_points(base_points, num_coins):
         coin_results.append(color)
         
     return total_points, coin_results
+
+
+def resolve_crash(player1_name, p1_base, p1_coins, player2_name, p2_base, p2_coins):
+    """
+    ระบบ Crash ระหว่าง 2 ฝ่าย
+    """
+    # คำนวณฝ่ายที่ 1
+    p1_total, p1_results = calculate_total_points(p1_base, p1_coins)
+    
+    # คำนวณฝ่ายที่ 2
+    p2_total, p2_results = calculate_total_points(p2_base, p2_coins)
+    
+    # สรุปผล
+    winner = None
+    if p1_total > p2_total:
+        winner = player1_name
+    elif p2_total > p1_total:
+        winner = player2_name
+    else:
+        winner = "เสมอ"
+        
+    return {
+        "player1": {
+            "name": player1_name,
+            "base_points": p1_base,
+            "coins": p1_coins,
+            "coin_results": p1_results,
+            "total_points": p1_total
+        },
+        "player2": {
+            "name": player2_name,
+            "base_points": p2_base,
+            "coins": p2_coins,
+            "coin_results": p2_results,
+            "total_points": p2_total
+        },
+        "winner": winner
+    }
